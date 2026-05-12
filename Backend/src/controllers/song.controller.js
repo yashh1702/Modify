aad2349e7f3af5d12b876873f602391b414c6fd6
yashh1702 <yagrawal944@gmail.com>
2026-05-13 02:31:10 +0530

    feat: Implement song upload and retrieval functionality with ID3 tag processing

--- Backend/src/controllers/song.controller.js
+++ Backend/src/controllers/song.controller.js
@@ -1,2 +1,50 @@
-const songModel = require("../models/song.model.js")
+const songModel = require("../models/song.model.js");
+const storageService = require("../services/storage.service.js");
+const id3 = require("node-id3");
 
+async function uploadSong(req, res) {
+  const songBuffer = req.file.buffer;
+  const { mood } = req.body;
+
+  const tags = id3.read(songBuffer);
+
+  const [songFile, posterFile] = await Promise.all([
+    storageService.uploadFile({
+      buffer: songBuffer,
+      filename: tags.title + ".mp3",
+      folder: "/moodify/songs",
+    }),
+    storageService.uploadFile({
+      buffer: tags.image.imageBuffer,
+      filename: tags.title + ".jpeg",
+      folder: "/moodify/posters",
+    }),
+  ]);
+
+  const song = await songModel.create({
+    title: tags.title,
+    url: songFile.url,
+    posterUrl: posterFile.url,
+    mood,
+  });
+
+  res.status(201).json({
+    message: "Song created Succssfully",
+    song,
+  });
+}
+
+async function getSong(req, res) {
+  const { mood } = req.query;
+
+  const song = await songModel.findOne({
+    mood,
+  });
+
+  res.status(200).json({
+    message: "Song Fetched Successfully",
+    song,
+  });
+}
+
+module.exports = { uploadSong, getSong };
--- Backend/src/routes/song.route.js
+++ Backend/src/routes/song.route.js
@@ -1,6 +1,9 @@
-const express = require("express")
-const songRouter = express.Router()
+const express = require("express");
+const songRouter = express.Router();
+const { uploadSong, getSong } = require("../controllers/song.controller.js");
+const upload = require("../middleware/upload.middleware.js");
 
+songRouter.post("/", upload.single("song"), uploadSong);
+songRouter.get("/", getSong);
 
-module.exports = songRouter
- 
+module.exports = songRouter;
